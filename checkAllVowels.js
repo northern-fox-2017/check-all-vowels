e5c59aa33939c3518772e898111c8999c9ea93a0
angrha <angrha@gmail.com>
2017-11-14 14:37:30 +0700

    belom pakai indexOf, ntar di ganti. aang

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,16 @@
+function checkVowel(kata) {
+  let vowel = 'aiueo'
+  let hasil = ''
 
+  for (let i = 0; i < kata.length; i++) {
+    for (let j = 0; j < vowel.length; j++) {
+      if(kata[i] === vowel[j]){
+        hasil += kata[i]
+      }
+    }
+  }
+  return hasil.length === kata.length
+}
+console.log(checkVowel('aiueo'));
+console.log(checkVowel('sudoku'));
+console.log(checkVowel('jsracer'));
