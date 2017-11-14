65ddfddb3a24ac3813f5ae95b02195c3a5676fc0
ariefmanda <arief.manda57@gmail.com>
2017-11-14 14:51:43 +0700

    Done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,14 @@
+function checkAllVowel(string) {
+    let vocal=['a','i','u','e','o','A','I','U','E','O'],checkSum=0;
+    for(let i=0;i<string.length;i++){
+        for(let j=0;j<vocal.length;j++){
+            if(string[i]===vocal[j]){
+                checkSum++
+            }
+        }
+    }
+    return string.length===checkSum;
+}
 
+console.log(checkAllVowel('aAuE')); //true
+console.log(checkAllVowel('aAruE')); //false
