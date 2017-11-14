6251e016eda4095e76e716ce55e681508bd3c6ef
yofri <yofriadiyahya@gmail.com>
2017-11-14 17:47:39 +0700

    Finished

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,6 +1,7 @@
 const vowel = 'aiueo';
 
 const checkVowel = (str) => {
+  str = str.toLowerCase();
   for (let i = 0; i < str.length; i++) {
     if (vowel.indexOf(str[i]) === -1) return false;
   }
@@ -8,4 +9,4 @@ const checkVowel = (str) => {
   return true;
 }
 
-console.log(checkVowel('baca'));
+console.log(checkVowel('Aa'));
