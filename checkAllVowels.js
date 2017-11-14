b2468758ed0cc644785bc82f348c19f9629cc2f7
rezaalfarabi <anangreza18@gmail.com>
2017-11-14 19:19:37 +0700

    release 0

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,17 @@
+function checkAllVowel(str) {
+    let vowel = 'aiueo'
+    let strLow = str.toLowerCase()
+    for (let i = 0; i < strLow.length; i++) {
+        if(vowel.indexOf(strLow[i]) === -1) {
+            return false
+        } 
+    }
+    return true
+}
 
+//test case
+console.log(checkAllVowel('AiuEo'))
+console.log(checkAllVowel('aiueo'))
+console.log(checkAllVowel('AIUEO'))
+console.log(checkAllVowel('aiUEHDgf'))
+console.log(checkAllVowel('AiUeO'))
