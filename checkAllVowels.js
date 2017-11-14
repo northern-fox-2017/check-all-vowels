9a83898a56077aabc7b31d034e7c1794f986bbdb
riskinputra <riski.n.putra@gmail.com>
2017-11-14 16:53:52 +0700

    first commit

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,16 @@
+function checkAllVowel(str) {
+  let vowel  = ['a','i','u','e','o'];
+  let newStr = ''
 
+  for (var i = 0; i < str.length; i++) {
+    for (var j = 0; j < vowel.length; j++) {
+      if (str[i] == vowel[j] ) {
+        newStr += str[i]
+      }
+    }
+  }
+
+  return newStr == str;
+}
+console.log(checkAllVowel('bibo'));
+console.log(checkAllVowel('oiao'));
