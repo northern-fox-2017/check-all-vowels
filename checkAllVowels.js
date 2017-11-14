f2abc0af4d8d7636a1072e1c9a3d9032ec458470
yofri <yofriadiyahya@gmail.com>
2017-11-14 11:31:22 +0700

    Finished checkAllVowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,11 @@
+const vowel = 'aiueo';
 
+const checkVowel = (str) => {
+  for (let i = 0; i < str.length; i++) {
+    if (vowel.indexOf(str[i]) === -1) return false;
+  }
+
+  return true;
+}
+
+console.log(checkVowel('baca'));
