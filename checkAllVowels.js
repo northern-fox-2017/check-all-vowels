c4f22138a297ef0642812080ccb3e458ba347185
hacktiv8 <hacktiv8@Sabtas-Mac-mini.local>
2017-11-14 17:48:48 +0700

    memperbaiki huruf besar, looping 1 kali

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,21 +1,14 @@
 function cekVowel (kata){
   var vowel = ['a','i','u','e','o'];
-  var huruf = kata.split('');
+  var huruf = kata.toLowerCase().split('');
   var kataVowel = '';
 
-
   for (var i = 0; i < huruf.length; i++) {
-    for (var j = 0; j < vowel.length; j++){
-      if(huruf[i] === vowel[j]){
-        kataVowel += huruf[i]
-      }
+    if(vowel.indexOf(huruf[i]) === -1){
+      return false;
+    } else {
+      return true;
     }
-  }
-
-  if(kataVowel.length === kata.length){
-    return true;
-  } else {
-    return false;
   }
 // 1. bikin var huruf yang berisikan split kata
 // 2. bikin var vowel yang berisikan kata vowel
@@ -27,6 +20,6 @@ function cekVowel (kata){
 }
 
 
-console.log(cekVowel('aiueo')); // TRUE
-console.log(cekVowel('gelas')); // FALSE
-console.log(cekVowel('ieuoeiueoiiuoa')); // TRUE
+console.log(cekVowel('aiuEo')); // TRUE
+console.log(cekVowel('geGlas')); // FALSE
+console.log(cekVowel('ieuoeiueEOUiiuoa')); // TRUE
