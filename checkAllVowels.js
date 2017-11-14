ba2d2fb36732de35d9da628819a1ee6188b33c64
idabagusangga <angga.idabagus@gmail.com>
2017-11-14 14:23:49 +0700

    checker done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,21 @@
-
+function vowelChecker(str){
+  let abjad = 'aiueo'
+  let arrAbjad = abjad.split('')
+  // return arrAbjad
+  for(var i = 0; i < arrAbjad.length;i++){
+    for(var j = 0;j < str.length;j++){
+      if(arrAbjad[i] == str[j]){
+        arrAbjad.splice(i,1)
+      }
+    }
+  }
+  console.log(arrAbjad)
+  if(arrAbjad.length == 0){
+    return true
+  }
+  else{
+    return false
+  }
+}
+console.log(vowelChecker('bbhahajsncauisncqwjldnajksbahklbaschklwbixowa')) //false (no 'u' nor 'e')
+console.log (vowelChecker('dhsjakdlhasjdklasaiueobahdjksahdsjiainchia@')) //true (all vowels used)
