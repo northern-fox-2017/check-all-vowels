6030d21dbb3fe578f876c8a0ba1841c44ad25096
dorman99 <hafizh.abdillahhh@gmail.com>
2017-11-14 17:27:54 +0700

    done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,20 @@
+function checkAllVowel(str){
+const vowel = ('aiueo')
+let kondisi ; 
+for (let i = 0; i < str.length; i++) {
+    if (vowel.indexOf(str[i]) === -1){
+    return kondisi = false;
 
+    }
+    else {
+        kondisi = true;
+    }
+   
+}
+return kondisi;
+
+}
+
+console.log(checkAllVowel('iiioioioioiioioiwioi'))//false
+console.log(checkAllVowel('diuiaoow')) //false
+console.log(checkAllVowel('iuuaao'))//true
