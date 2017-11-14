4636652144fff572ccdb50106e03d6325bd71d90
Andrey Amrulloh <andrey.amrulloh@gmail.com>
2017-11-14 18:12:13 +0700

    Replace Origin 1 Andrey Amrulloh CheckAllVowels.js

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,4 +1,14 @@
 function checkVowels(string){
-	return (string === string.toUpperCase()) ? true : false;
+	var arr = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'];
+	var lowerCase = string.toLowerCase();
+	var count = 0;
+	debugger
+	for(var i = 0; i < arr.length; i++){
+		if(lowerCase.indexOf(arr[i]) !== -1){
+			count = 1;
+		}
+	}
+	return (count === 0) ? true : false;
+	
 }
-console.log(checkVowels('TEST'))
+console.log(checkVowels('AIUOaeuiiiiOOIUIiUIAIUOaeuiiiiOOIUIiUIAIUOaeuiiiiOOIUIiUIAIUOaeuiiiiOOIUIiUI'));
