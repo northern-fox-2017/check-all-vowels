339f29696bfb9222e9f1fd2a9478898086fd5696
Fajar Patappari <top.sick.red@gmail.com>
2017-11-14 15:32:47 +0700

    Release 0: Selasa

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,14 @@
 
+function allVowels(str){
+  var vowels = 'aiueo'
+
+  for(var char of str){
+    if(vowels.indexOf(char) === -1){
+      return false
+    }
+  }
+  return true
+}
+
+console.log(allVowels('aaaaaiiiiu'))
+console.log(allVowels('vbsdsadsaqqq'))
