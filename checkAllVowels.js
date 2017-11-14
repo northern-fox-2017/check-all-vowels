c833a7ab55132fd9e3a561f3b640f16d0fa92c10
febriliando <febriliandoputra@gmail.com>
2017-11-14 20:02:19 +0700

    checkallVowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,15 @@
 
+function checkAllVowels(str){
+  let check = ['a','i','u','e','o'];
+
+  for(let i in str){
+    if(check.indexOf(str[i]) == -1){
+      return false
+    }
+  }
+  return true
+}
+
+console.log(checkAllVowels('aiueo'));
+console.log(checkAllVowels('bairueo'));
+console.log(checkAllVowels('oieua'));
