5bf2d96070b8e8dc11987a64cfbe9a112bc61a51
agungpangestu <agung_pangestu83@ymail.com>
2017-11-14 14:19:38 +0700

    Done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,18 @@
+let checkVowels = (input) => {
+   let vowel = ['a','i','u','e','o']
+  //  console.log();
+  let counter = 0
+  for (i of input ){
+    // console.log(`ini i atas ${i}`);
+    for (j of vowel){
+      // console.log(`ini j bawah ${j}`);
+      if (i === j){
+        counter++
+      }
+    }
+  }
+  return (counter == input.length) ? true : false
+}
 
+
+console.log(checkVowels('aiueoaiueo'));
