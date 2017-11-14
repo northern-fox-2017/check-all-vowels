52bb31de317c4f0693c9d82f36261c359440fc27
Hacktiv8 <hacktiv8-6@Hacktiv8-6.local>
2017-11-14 15:04:04 +0700

    Release 0: Selasa

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,11 @@
+function checkVowel(str) {
+  var vowel = ['a', 'i', 'u', 'e', 'o']
+  for (var i = 0; i < str.length; i++) {
+    if (vowel.indexOf(str[i].toLowerCase()) == -1) {
+      return false;
+    }
+  }
+  return true;
+}
 
+console.log(checkVowel('1aaaA'))
